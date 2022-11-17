e63168a64b1d3b396a9f101e2c872f3531dedab9
eldarlrd <eldarlrd@protonmail.com>
2022-11-18 00:58:31 +0400

    feat: Initial design complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 // Boilerplate
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+import { faSquareTwitter } from "@fortawesome/free-brands-svg-icons";
 import "./App.scss";
-/* NOTE: URLs Go Here */
+const twitterURL = "https://twitter.com/";
 // Render
 export default function App() {
   return (
-    <>
-    </>
+    <div id="quote-box">
+      <h2 id="text">
+        <FontAwesomeIcon icon={faQuoteLeft}/> It's the wood that should fear your hand, not the other way around. No wonder you can't do it, you acquiesce to defeat before you even begin.
+      </h2>
+      <p id="author">- Pai Mei</p>
+      <button id="new-quote">New Quote</button>
+      <a
+        id="tweet-quote"
+        target="_blank"
+        rel="noreferrer"
+        href={twitterURL}>
+        <FontAwesomeIcon icon={faSquareTwitter}/>
+      </a>
+    </div>
   );
-};
+}
