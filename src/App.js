f0cfc8b37d11c2322fdea286c87e42ea74099085
eldarlrd <eldarlrd@protonmail.com>
2022-11-18 14:20:51 +0400

    feat: Add API & Randomizer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 // Boilerplate
 import React from "react";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 import { faSquareTwitter } from "@fortawesome/free-brands-svg-icons";
 import "./App.scss";
+// URLs
+const quoteURL = "https://type.fit/api/quotes";
 const twitterURL = "https://twitter.com/";
-// Render
+// State Declaration
 export default function App() {
+  const [ quote, setQuote ] = React.useState(null);
+// Randomizer
+  const roll = Math.round((Math.random() * 1000));
+  const [ random, setRandom ] = React.useState(roll);
+  const newRoll = () => setRandom(roll);
+// API Fetch
+  React.useEffect(() => {
+    axios.get(quoteURL).then(response => {
+      setQuote(response.data);
+    });
+  }, []);
+// Render
   return (
     <div id="quote-box">
       <h2 id="text">
-        <FontAwesomeIcon icon={faQuoteLeft}/> It's the wood that should fear your hand, not the other way around. No wonder you can't do it, you acquiesce to defeat before you even begin.
+        <FontAwesomeIcon icon={faQuoteLeft}/> { quote
+                                                ? quote[random].text
+                                                : "" }
       </h2>
-      <p id="author">- Pai Mei</p>
-      <button id="new-quote">New Quote</button>
+      <p id="author">- { quote
+                          ? quote[random].author
+                          : "" }
+      </p>
+      <button id="new-quote" onClick={newRoll}>New Quote</button>
       <a
         id="tweet-quote"
         target="_blank"
