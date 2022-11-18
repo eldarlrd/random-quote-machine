8f6b9d9b85985b18e219aab08e3f3aca50afd63f
eldarlrd <eldarlrd@protonmail.com>
2022-11-18 17:20:06 +0400

    feat: Finalize Static Design, Add Favicon

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import { faSquareTwitter } from "@fortawesome/free-brands-svg-icons";
 import "./App.scss";
 // URLs
 const quoteURL = "https://type.fit/api/quotes";
-const twitterURL = "https://twitter.com/";
+const githubURL = "https://github.com/eldarlrd";
+const twitterURL = "https://twitter.com/intent/tweet";
 // State Declaration
 export default function App() {
   const [ quote, setQuote ] = React.useState(null);
 // Randomizer
-  const roll = Math.round((Math.random() * 1000));
+  const roll = Math.floor((Math.random() * 1000));
   const [ random, setRandom ] = React.useState(roll);
   const newRoll = () => setRandom(roll);
 // API Fetch
@@ -23,24 +24,40 @@ export default function App() {
   }, []);
 // Render
   return (
+    <>
+    <header>
+      Random Quote Machine
+      <br />
+      by <a
+        target="_blank"
+        rel="noreferrer"
+        href={githubURL}>
+        eldarlrd</a>
+    </header>
     <div id="quote-box">
       <h2 id="text">
-        <FontAwesomeIcon icon={faQuoteLeft}/> { quote
-                                                ? quote[random].text
-                                                : "" }
+        <FontAwesomeIcon icon={faQuoteLeft}/> {
+        quote
+        ? quote[random].text
+        : "" }
       </h2>
-      <p id="author">- { quote
-                          ? quote[random].author
-                          : "" }
+      <p id="author">- {
+        quote
+        ? quote[random].author
+          ? quote[random].author
+          : "Unknown"
+        : "" }
       </p>
       <button id="new-quote" onClick={newRoll}>New Quote</button>
       <a
         id="tweet-quote"
+        title="Tweet this quote!"
         target="_blank"
         rel="noreferrer"
         href={twitterURL}>
         <FontAwesomeIcon icon={faSquareTwitter}/>
       </a>
     </div>
+    </>
   );
 }
